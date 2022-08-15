8eca21dc083ab2793b1f6f7bb24fe1c591f2c9c9
parhelion <55342306+prahelion@users.noreply.github.com>
2022-08-15 08:35:35 -0400

    Update heliocentric.lua

--- heliocentric.lua
+++ heliocentric.lua
@@ -3,16 +3,23 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Green
 
 local Player = game.Players.LocalPlayer
 
+local Players = game:GetService("Players")
+
 local UI = library.new("A Heliocentric Time")
 
+
+--// Pages
 local playerPage = UI:addPage("Player", 8950959091)
+local farmPage = UI:addPage("Farming", 10595805195)
+local teleportPage = UI:addPage("Teleports")
 
+
+--// Character Page
 local characterSection = playerPage:addSection("Character")
-
 
 game:GetService("UserInputService").InputBegan:Connect(function(Input, Proc)
 	if Proc then return end
-	if Input.KeyCode == Enum.KeyCode.Insert then
+	if Input.KeyCode == Enum.KeyCode.End then
 		UI:toggle()
 	end
 end)
@@ -94,107 +101,98 @@ emoteSection:addDropdown("Emote List",
 	end)
 
 
-local farmPage = UI:addPage("Farming", 10595805195)
+
+
+--// Farming Page
 
 local manualSection = farmPage:addSection("Manual")
 
+local chestRunning = false
 manualSection:addButton("Collect Chests", function()
-	pcall(function()
-
-		local Character = Player.Character
-
-		local oldpos = Character.HumanoidRootPart.Position
-
-		local Chests = game:GetService("Workspace").ItemSpawns.Chests
-
-		for _, Location in pairs(Chests:GetChildren()) do
-			if Location:FindFirstChild("Chest") then 
-				local Chest = Location.Chest
-
-				local Prompt = Chest.RootPart.ProximityAttachment.Interaction
-
-				Character.HumanoidRootPart.CFrame = Location.CFrame
-				wait(0.2)
-				fireproximityprompt(Prompt)
-				wait()
-			end
+	if chestRunning then return end
+	chestRunning = true
+	
+	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
+		
+	for _, Descendant in pairs(workspace.ItemSpawns.Chests:GetDescendants()) do
+		if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+			HRP.CFrame = Descendant.Parent.Parent.CFrame
+			task.wait(.2)
+			fireproximityprompt(Descendant)
+			task.wait()
 		end
-		Character.HumanoidRootPart.Anchored = true
-		Character.HumanoidRootPart.Position = oldpos
-		Character.HumanoidRootPart.Anchored = false
-	end)
+	end
+	
+	HRP.CFrame = currentPosition
+	chestRunning = false
 end)
 
+local itemRunning = false
 manualSection:addButton("Collect Items", function()
+	if itemRunning then return end
+	itemRunning = true
 
 	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
 
-	local oldpos = Character.HumanoidRootPart.Position
-
-	local Items = workspace.ItemSpawns.StandardItems:GetDescendants()
-
-	for _, desc in pairs(Items) do
-		if desc.Name == "Interaction" and desc.Parent.name == "ProximityAttachment" then
-			Character.HumanoidRootPart.CFrame = desc.Parent.Parent.CFrame
-			wait(0.2)
-			fireproximityprompt(desc)
-			wait()
-		end 
+	for _, Descendant in pairs(workspace.ItemSpawns.StandardItems:GetDescendants()) do
+		if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+			HRP.CFrame = Descendant.Parent.Parent.CFrame
+			task.wait(.2)
+			fireproximityprompt(Descendant)
+			task.wait()
+		end
 	end
 
-
-	Character.HumanoidRootPart.Anchored = true
-	Character.HumanoidRootPart.Position = oldpos
-	Character.HumanoidRootPart.Anchored = false
+	HRP.CFrame = currentPosition
+	itemRunning = false
 end)
 
+local debrisRunning = false
 manualSection:addButton("Collect Debris", function()
-	pcall(function()
+	if debrisRunning then return end
+	debrisRunning = true
+
 	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
 
-	local oldpos = Character.HumanoidRootPart.Position
-
-	local Items = workspace.ItemSpawns["Sand Debris"]:GetDescendants()
-
-	for _, desc in pairs(Items) do
-		if desc.Name == "Interaction" and desc.Parent.name == "ProximityAttachment" then
-			Character.HumanoidRootPart.CFrame = desc.Parent.Parent.CFrame
-			wait(0.2)
-			fireproximityprompt(desc)
-			wait(3.4)
-		end 
+	for _, Descendant in pairs(workspace.ItemSpawns["Sand Debris"]:GetDescendants()) do
+		if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+			HRP.CFrame = Descendant.Parent.Parent.CFrame
+			task.wait(.2)
+			fireproximityprompt(Descendant)
+			task.wait(4)
+		end
 	end
 
-
-	Character.HumanoidRootPart.Anchored = true
-	Character.HumanoidRootPart.Position = oldpos
-		Character.HumanoidRootPart.Anchored = false
-	end)
+	HRP.CFrame = currentPosition
+	debrisRunning = false
 end)
 
+local meteorRunning = false
 manualSection:addButton("Collect Meteors", function()
-	pcall(function()
+	if meteorRunning then return end
+	meteorRunning = true
 
 	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
 
-	local oldpos = Character.HumanoidRootPart.Position
-
-	local Items = workspace.ItemSpawns["Meteors"]:GetDescendants()
-
-	for _, desc in pairs(Items) do
-		if desc.Name == "Interaction" and desc.Parent.name == "ProximityAttachment" then
-			Character.HumanoidRootPart.CFrame = desc.Parent.Parent.CFrame
-			wait(0.2)
-			fireproximityprompt(desc)
-			wait()
-		end 
+	for _, Descendant in pairs(workspace.ItemSpawns.Meteors:GetDescendants()) do
+		if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+			HRP.CFrame = Descendant.Parent.Parent.CFrame
+			task.wait(.2)
+			fireproximityprompt(Descendant)
+			task.wait()
+		end
 	end
 
-
-	Character.HumanoidRootPart.Anchored = true
-	Character.HumanoidRootPart.Position = oldpos
-		Character.HumanoidRootPart.Anchored = false
-		end)
+	HRP.CFrame = currentPosition
+	meteorRunning = false
 end)
 
 
@@ -208,253 +206,168 @@ local autoItem = false
 local autoChest = false
 local autoDebris= false
 
-workspace.ItemSpawns.Meteors.DescendantAdded:Connect(function(Descendant)
-	if not autoMeteor then return end
-	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
-		
-	print("hilarious meteor")
-	local currentPosition = Player.Character.HumanoidRootPart.Position
-		
-	wait(2)
-	Player.Character.HumanoidRootPart.CFrame = Descendant.Parent.Parent.Parent.CFrame
-	wait(0.2)
-	fireproximityprompt(Descendant)
-	wait()
-
-	Player.Character.HumanoidRootPart.Anchored = true
-	Player.Character.HumanoidRootPart.Position = currentPosition
-	Player.Character.HumanoidRootPart.Anchored = false
-	end
-end)
-
-automaticSection:addToggle("Auto Collect Meteor", false, function(value)
-	autoMeteor = value
-end)
-
--- ITEMS
-
-workspace.ItemSpawns.StandardItems.DescendantAdded:Connect(function(Descendant)
-	if not autoItem then return end
-	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
-
-		local currentPosition = Player.Character.HumanoidRootPart.Position
-
-		wait()
-		Player.Character.HumanoidRootPart.CFrame = Descendant.Parent.Parent.Parent.CFrame
-		wait(0.2)
-		fireproximityprompt(Descendant)
-		wait()
-
-		Player.Character.HumanoidRootPart.Anchored = true
-		Player.Character.HumanoidRootPart.Position = currentPosition
-		Player.Character.HumanoidRootPart.Anchored = false
-	end
+automaticSection:addToggle("Auto Loot Chest", false, function(value)
+	autoChest = value
 end)
 
 automaticSection:addToggle("Auto Collect Items", false, function(value)
 	autoItem = value
 end)
 
--- CHESTS
-
-workspace.ItemSpawns.Chests.DescendantAdded:Connect(function(Descendant)
-	if not autoChest then return end
-	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
-		print("interaction added ok")
-		local currentPosition = Player.Character.HumanoidRootPart.Position
-
-		wait()
-		Player.Character.HumanoidRootPart.CFrame = Descendant.Parent.Parent.CFrame
-		wait(0.2)
-		fireproximityprompt(Descendant)
-		wait()
-
-		Player.Character.HumanoidRootPart.Anchored = true
-		Player.Character.HumanoidRootPart.Position = currentPosition
-		Player.Character.HumanoidRootPart.Anchored = false
-	end
+automaticSection:addToggle("Auto Collect Meteors", false, function(value)
+	autoMeteor = value
 end)
 
-automaticSection:addToggle("Auto Loot Chests", false, function(value)
-	autoChest = value
-end)
-
--- DEBRIS
-
-workspace.ItemSpawns["Sand Debris"].DescendantAdded:Connect(function(Descendant)
-	if not autoDebris then return end
-	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
-
-		local currentPosition = Player.Character.HumanoidRootPart.Position
-
-		wait()
-		Player.Character.HumanoidRootPart.CFrame = Descendant.Parent.Parent.Parent.CFrame
-		wait(0.2)
-		fireproximityprompt(Descendant)
-		wait()
-
-		Player.Character.HumanoidRootPart.Anchored = true
-		Player.Character.HumanoidRootPart.Position = currentPosition
-		Player.Character.HumanoidRootPart.Anchored = false
-	end
-end)
-
-automaticSection:addToggle("Auto Collect Debris", false, function(value)
+automaticSection:addToggle("Auto Collect Sand Debris", false, function(value)
 	autoDebris = value
 end)
 
-
-
-
-
-
-
-local teleportPage = UI:addPage("Teleports")
-
-local utilitySection = teleportPage:addSection("Utility")
-
-local userSection = teleportPage:addSection("Players")
-
-function npclist()
-	local npcList = {}
-	for _, npc in pairs(workspace.NPCS:GetChildren()) do
-		if npc:FindFirstChild("HumanoidRootPart") then
-			npcList[npc.Name] = npc.Name
-		end
-	end
-	return npcList
-end
-
-utilitySection:addButton("Load Locations", function()
+-- Chests
+workspace.ItemSpawns.Chests.DescendantAdded:Connect(function(Descendant)
+	if not autoChest then return end
 	local Character = Player.Character
-	local oldpos = Character.HumanoidRootPart.CFrame
-	local locations = workspace.Map.GateLocations
-	for _, location in pairs(locations:GetChildren()) do
-		if location:IsA("Part") then
-			Character.HumanoidRootPart.CFrame = location.CFrame
-			wait(0.1)
-		end
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
+
+	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+		HRP.CFrame = Descendant.Parent.Parent.CFrame
+		task.wait(.2)
+		fireproximityprompt(Descendant)
+		task.wait()
+		HRP.CFrame = currentPosition
 	end
-	Character.HumanoidRootPart.Anchored = true
-	Character.HumanoidRootPart.CFrame = oldpos
-	Character.HumanoidRootPart.Anchored = false
 end)
 
+-- Items
+workspace.ItemSpawns.StandardItems.DescendantAdded:Connect(function(Descendant)
+	if not autoItem then return end
+	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
 
-local ps = game:GetService("Players")
+	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+		HRP.CFrame = Descendant.Parent.Parent.CFrame
+		task.wait(.2)
+		fireproximityprompt(Descendant)
+		task.wait()
+		HRP.CFrame = currentPosition
+	end
+end)
+
+-- Meteors
+workspace.ItemSpawns.Meteors.DescendantAdded:Connect(function(Descendant)
+	if not autoMeteor then return end
+	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
+
+	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+		task.wait(2)
+		HRP.CFrame = Descendant.Parent.Parent.CFrame
+		task.wait(.2)
+		fireproximityprompt(Descendant)
+		task.wait()
+		HRP.CFrame = currentPosition
+	end
+end)
+
+-- Debris
+workspace.ItemSpawns["Sand Debris"].DescendantAdded:Connect(function(Descendant)
+	if not autoDebris then return end
+	local Character = Player.Character
+	local HRP = Character.HumanoidRootPart
+	local currentPosition = HRP.CFrame
+
+	if Descendant.Name == "Interaction" and Descendant.Parent.Name == "ProximityAttachment" then
+		HRP.CFrame = Descendant.Parent.Parent.CFrame
+		task.wait(.2)
+		fireproximityprompt(Descendant)
+		task.wait()
+		HRP.CFrame = currentPosition
+	end
+end)
+
+--// Teleports
+
+local utilitySection = teleportPage:addSection("Utility")
+local userSection = teleportPage:addSection("Players")
+local npcSection = teleportPage:addSection("NPCs")
+local locationSection = teleportPage:addSection("Locations")
+
+-- Players
 
 function generatePlayerList()
 	local list = {}
-	for _, player in pairs(ps:GetPlayers()) do
+	for _, player in pairs(Players:GetPlayers()) do
 		list[player.Name] = player.Name
 	end
 	return list
 end
 
-local playerDropdown = userSection:addDropdown("Player List", generatePlayerList(), function(playerName)
-	if not workspace.Living:FindFirstChild(playerName) then return end
-
+function dropdownPressed(playerName)
 	pcall(function()
-		Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
+		local Character = Player.Character
+		if not workspace.Living:FindFirstChild(playerName) then return end 
 
-		userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-			if not workspace.Living:FindFirstChild(playerName) then return end
-
-			pcall(function()
-				Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-
-				userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-					if not workspace.Living:FindFirstChild(playerName) then return end
-
-					pcall(function()
-						Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-					end)
-				end)
-			end)
-		end)
+		Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
 	end)
-end)
-
-ps.PlayerAdded:Connect(function()
-	userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-		if not workspace.Living:FindFirstChild(playerName) then return end
-
-		pcall(function()
-			Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-
-			userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-				if not workspace.Living:FindFirstChild(playerName) then return end
-
-				pcall(function()
-					Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-
-					userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-						if not workspace.Living:FindFirstChild(playerName) then return end
-
-						pcall(function()
-							Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-						end)
-					end)
-				end)
-			end)
-		end)
-	end)
-end)
-
-ps.PlayerRemoving:Connect(function()
-	userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-		if not workspace.Living:FindFirstChild(playerName) then return end
-
-		pcall(function()
-			Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-
-			userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-				if not workspace.Living:FindFirstChild(playerName) then return end
-
-				pcall(function()
-					Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-
-					userSection:updateDropdown(playerDropdown, "Player List", generatePlayerList(), function(playerName)
-						if not workspace.Living:FindFirstChild(playerName) then return end
-
-						pcall(function()
-							Player.Character.HumanoidRootPart.CFrame = workspace.Living[playerName].HumanoidRootPart.CFrame
-						end)
-					end)
-				end)
-			end)
-		end)
-	end)
-end)
-
-local npcSection = teleportPage:addSection("NPCs")
-
-
-local npcdropdown = npcSection:addDropdown("NPC List", npclist(), function(npcName)
-	Player.Character.HumanoidRootPart.CFrame = workspace.NPCS[npcName].HumanoidRootPart.CFrame
-end)
-
-utilitySection:addButton("Refresh NPCs", function()
-	npcSection:updateDropdown(npcdropdown, "NPC List", npclist(), function(npcName)
-		Player.Character.HumanoidRootPart.CFrame = workspace.NPCS[npcName].HumanoidRootPart.CFrame
-	end)
-end)
-
-local locationSection = teleportPage:addSection("Locations")
-
-function locationlist()
-	local list = {}
-	for _, child in pairs(workspace.Map.GateLocations:GetChildren()) do
-		if child:IsA("Part") then
-			list[child.Name] = child.Name
-		end
-	end
-	return list
 end
 
-locationSection:addDropdown("Locations", locationlist(), function(locationName)
-	Player.Character.HumanoidRootPart.CFrame = workspace.Map.GateLocations[locationName].CFrame
+local playerDropdown = userSection:addDropdown("Player List", generatePlayerList(), dropdownPressed)
+
+function refreshPlayerList()
+	userSection:updateDropdown(playerDropdown, generatePlayerList(), dropdownPressed)
+end
+
+Players.PlayerAdded:Connect(refreshPlayerList)
+
+Players.PlayerRemoving:Connect(refreshPlayerList)
+
+-- NPCs
+
+function createNPCList()
+	local NPCList = {}
+	for _, NPC in pairs(workspace.NPCS:GetChildren()) do
+
+		if NPC:FindFirstChild("HumanoidRootPart") then NPCList[NPC.Name] = NPC.Name end
+
+	end
+	return NPCList
+end
+
+function npcDropdownPress(NPC)
+	local Character = Player.Character
+	
+	Character.HumanoidRootPart.CFrame = workspace.NPCS[NPC].HumanoidRootPart.CFrame
+end
+
+local npcListD = npcSection:addDropdown("NPC List", createNPCList(), npcDropdownPress)
+
+utilitySection:addButton("Refresh NPCs", function()
+	npcSection:updateDropdown(npcListD, "NPC List", createNPCList(), npcDropdownPress)
 end)
 
+-- Locations
+function createLocationList()
+	local locationList = {}
+	for _, Location in pairs(workspace.Map.GateLocations:GetChildren()) do
+		wait()
+		if Location:IsA("Part") then locationList[Location.Name] = Location.Name end
+
+	end
+	return locationList
+end
+
+function locationDropdownPress(Location)
+	local Character = Player.Character
+
+	Character.HumanoidRootPart.CFrame = workspace.Map.GateLocations[Location].CFrame
+end
+
+local locationDrop = locationSection:addDropdown("Location List", createLocationList(), locationDropdownPress)
+
+utilitySection:addButton("Refresh Locations", function()
+	npcSection:updateDropdown(locationDrop, "Location List", createLocationList(), locationDropdownPress)
+end)
 
 UI:SelectPage(playerPage, true)
